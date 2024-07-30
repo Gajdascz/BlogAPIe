6591266b59f0cddd1578312a1ab04b84aaf0b9cd
gajdascz <gajdascz@gmail.com>
2024-07-29 22:16:46 -0400

    Config and structure

--- repo/cli/commands/pkg/pkg.ts
+++
@@ -1,7 +0,0 @@
-import { Command } from 'commander';
-
-const pkg = new Command('pkg');
-
-pkg
-  .command('add <type> <packageName>')
-  .description('Builds a new package with the specified type');
